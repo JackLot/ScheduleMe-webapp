d21b20790e79f8844992b9b9a3de67f220d56aeb
jlotkows <jacklotkowski@gmail.com>
2014-11-20 12:39:06 -0500

    First commit. Blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScheduleMeWebapp::Application.config.secret_token = '4bb592a2a4e33302dd9602eccee0301b23c5bc9b9c816cc4d51a153b5d950f159d86728a6c7866d283df05524377a04457407e7d64dac91a964f729339830f3c'
